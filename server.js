ce5994b87ee15515da8f85749a51355ac9516866
Rochungnunga Hrangch <hrangchalsolutions@gmail.com>
2021-04-05 12:11:06 +0530

    Ported!

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ require('./routes/chengrang.route.js')(app);
 // A chung a code nen hian a in ang chiah..  express hi ROUTE nan kan hmang dawn tihna.
 
 
-const port = process.environment.PORT || 3066;
+const port = process.environment.Port || 3066;
 
 app.listen(port,()=>{
   console.log("Server Connected");
